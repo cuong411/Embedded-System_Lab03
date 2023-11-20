7f9257daf9489ad7ad59266049485c9e0a35dec5
cuong411 <cuongtruong29122002@gmail.com>
2023-11-20 15:11:30 +0700

    update main.c

--- main/lab3.c
+++ main/lab3.c
@@ -7,11 +7,17 @@
 #include <stdio.h>
 #include "FreeRTOSConfig.h"
 
+int i = 0;
+TaskHandle_t xHandle;
+
 void task1(void *parameter)
 {
     while (true)
     {
         printf("task1 is running\n");
+        i++;
+        if (i == 200)
+            vTaskSuspend(xHandle);
     }
     vTaskDelete(NULL);
 }
@@ -20,7 +26,10 @@ void task2(void *parameter)
 {
     while (true)
     {
-        printf("task2 is running\n");
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+        printf("task2 starts\n");
+        printf("task2 ends\n");
+        vTaskDelay(200 / portTICK_PERIOD_MS);
     }
     vTaskDelete(NULL);
 }
@@ -29,16 +38,17 @@ void task3(void *parameter)
 {
     while (true)
     {
+        vTaskDelay(50 / portTICK_PERIOD_MS);
         printf("task3 starts\n");
         printf("task3 ends\n");
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        vTaskDelay(200 / portTICK_PERIOD_MS);
     }
     vTaskDelete(NULL);
 }
 
 void app_main()
 {
-    xTaskCreate(task3, "task3", 2048, NULL, 2, NULL);
-    xTaskCreate(task1, "task1", 2048, NULL, 1, NULL);
-    xTaskCreate(task2, "task2", 2048, NULL, 1, NULL);
+    xTaskCreate(task1, "task1", 2048, NULL, 1, &xHandle);
+    xTaskCreate(task2, "task2", 2048, NULL, 2, NULL);
+    xTaskCreate(task3, "task3", 2048, NULL, 3, NULL);
 }
